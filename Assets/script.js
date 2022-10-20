2c4fdf3ebb9f194873ba04af66278a0fd874e3c3
Husaam NT <husnaeem@gmail.com>
2022-10-20 13:44:52 +0100

    created start button function

--- Assets/script.js
+++ Assets/script.js
@@ -4,9 +4,17 @@ const questionOneEl  = $("#option1")
 const questionTwoEl  = $("#option2")
 const questionThreeEl  = $("#option3")
 const questionFourEl  = $("#option4")
-const questionAllButtonsEl = $(".optionButtons")
+const questionAllButtonsEl = $(".optionButton")
 
+$(questionAllButtonsEl).hide();
 
-startButtonEl.on("click", function(){
-
+const startGame = startButtonEl.on("click", function(){
+    $(questionAllButtonsEl).show();
+    $(startButtonEl).hide();
+    questionHeader.text("Which one is not a data type?")
+    questionOneEl.text("1. Boolean")
+    questionTwoEl.text("2. Surd")
+    questionThreeEl.text("3. Integer")
+    questionFourEl.text("4. Undefined")
+    
 })
