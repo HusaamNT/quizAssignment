66125e99ee466ea9b7ffab48185177334da28551
Husaam NT <husnaeem@gmail.com>
2022-10-25 11:40:47 +0100

    Begun adding score elements

--- Assets/script.js
+++ Assets/script.js
@@ -7,24 +7,27 @@ const questionThreeEl  = $("#option3")
 const questionFourEl  = $("#option4")
 const questionAllButtonsEl = $(".optionButton")
 const timeElement = $(".time")
-
+const allElements = $("body.children")
 
 let secondsLeft = 30
 $(retryButtonEl).hide();
 $(questionAllButtonsEl).hide();
+const points = 0;
 
 const startGame = startButtonEl.on("click", function(){
     $(questionAllButtonsEl).show();
     $(startButtonEl).hide();
-    questionHeader.text("Which one is not a data type?")
-    questionOneEl.text("1. Boolean")
-    questionTwoEl.text("2. Surd")
-    questionThreeEl.text("3. Integer")
-    questionFourEl.text("4. Undefined")
+    questionHeader.text("Which one is not a data type?");
+    questionOneEl.text("1. Boolean");
+    questionTwoEl.text("2. Surd");
+    questionThreeEl.text("3. Integer");
+    questionFourEl.text("4. Undefined");
     if (questionTwoEl){
 
+
     }
-})
+});
+
 
 
 function setTime() {
@@ -33,11 +36,18 @@ function setTime() {
         timeElement.text = secondsLeft + "s left!"
         if(secondsLeft === 0 ){
             clearInterval(gameTimer);
+            endGameScreen();
+
 
         }
     })
 }
 
 function endGameScreen(){
+    $()
+}
+
+
+function addPoints(){
 
 }
